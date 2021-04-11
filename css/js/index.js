1895443e57dbde4b6627dbdc3f86616c78584d92
ANDRII ALENIN <mkalenin@gmail.com>
2021-04-11 11:16:12 +0300

    Update index.js

--- css/js/index.js
+++ css/js/index.js
@@ -19,6 +19,56 @@ class Timer {
     }
 
     start() {
-            if (this.isActive) {
-                return;
-            }
+        if (this.isActive) {
+            return;
+        }
+
+        const startTime = new Date('July 10 2021');
+        console.log(startTime);
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            // console.log('currentTime', currentTime);
+            const deltaTime = currentTime - startTime;
+            // console.log('deltaTime', deltaTime);
+            const time = this.getTimeComponents(-deltaTime);
+
+            this.onTick(time);
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        const time = this.getTimeComponents(0);
+        this.onTick(time);
+    }
+
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(
+            Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        );
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return { days, hours, mins, secs };
+    }
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+}
+
+const timer = new Timer({
+    onTick: updateClockface,
+});
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
